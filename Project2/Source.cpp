121422e97b6604eee1c176bdbb96db60a144a874
jhhope1 <jhhope1@naver.com>
2019-12-07 23:22:42 +0900

    Nothing changed

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -7,7 +7,7 @@ int main() {
 
 	OB.push_back(new Object(Rigidbody(1, vec(1.0833, 0.833, 0.416)), vec(0, 0, 4), vec(0.3, 0, 0), vec(0, 0.6, 0), tensor(), vec(0.5, 0.5, 4)));
 
-	for (int i = 1; i < 5 ; i++) {
+	for (int i = 1; i < 4 ; i++) {
 		OB.push_back(new Object(Rigidbody(1, vec(1.0833, 0.833, 0.416)), vec(0, 0, 4*(i+1)), vec(0, 0., 0), vec(0, 0, 0), tensor(), vec(0.5, 0.5, 4)));
 	}
 	Environment env(0.01,false);
