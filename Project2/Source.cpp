c257d021c810ad789724322ab23cd431206a5e3a
jhhope1 <jhhope1@naver.com>
2019-12-07 23:20:16 +0900

    Access denied...

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -2,14 +2,32 @@
 #include "Geometry.h"
 #include "Lagrangian.h"
 #include <random>
+class Complex {
+public:
+	double re;
+	double im;
+	Complex() {
+		re = 0;
+		im = 0;
+	}
+	Complex(double re, double im) {
+		this->re = re;
+		this->im = im;
+	}
+	Complex operator *(Complex C) {
+		return Complex(C.re * re - C.im * im, C.re * im + C.im * re);
+	}
+};
 int main() {
 	vector <Object*> OB;
+	int n = 3;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < (1 << n); j++) {
 
-	OB.push_back(new Object(Rigidbody(1, vec(1.0833, 0.833, 0.416)), vec(0, 0, 4), vec(0.3, 0, 0), vec(0, 0.6, 0), tensor(), vec(0.5, 0.5, 4)));
-
-	for (int i = 1; i < 5 ; i++) {
-		OB.push_back(new Object(Rigidbody(1, vec(1.0833, 0.833, 0.416)), vec(0, 0, 4*(i+1)), vec(0, 0., 0), vec(0, 0, 0), tensor(), vec(0.5, 0.5, 4)));
+		}
 	}
+	OB.push_back(new Object());
+
 	Environment env(0.01,false);
 	env.push_back(OB);
 	env.simulate(10000);
